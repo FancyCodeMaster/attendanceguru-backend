2d3fb790a96598b1698053ae325a16a2eaa48fc9
FancyCodeMaster <fancycodemaster@gmail.com>
2023-09-27 22:04:23 +0545

    changes for railway

--- src/main/java/com/arun/ag_backend/Entities/Attendance.java
+++ src/main/java/com/arun/ag_backend/Entities/Attendance.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 @Entity
@@ -27,6 +28,8 @@ public class Attendance {
     private ClassRoutine classRoutine;
 
     private LocalDate date;
+
+//    private DayOfWeek day;
 
     private String status;
 }
--- src/main/java/com/arun/ag_backend/Repo/TeacherRepo.java
+++ src/main/java/com/arun/ag_backend/Repo/TeacherRepo.java
@@ -1,9 +1,7 @@
 package com.arun.ag_backend.Repo;
 
+import com.arun.ag_backend.Entities.*;
 import com.arun.ag_backend.Entities.Class;
-import com.arun.ag_backend.Entities.Student;
-import com.arun.ag_backend.Entities.Subject;
-import com.arun.ag_backend.Entities.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,6 +15,8 @@ public interface TeacherRepo extends JpaRepository<Teacher, Integer > {
     @Query("SELECT t FROM Teacher t WHERE t.user.email = :email")
     Optional<Teacher> findByUserEmail(@Param("email") String email);
 
+    @Query("SELECT u.name , u.email FROM Users u WHERE u.role = 'teacher'")
+    List<Object> findAllTeachers();
 
     @Query("SELECT   c FROM Class c " +
             "JOIN TeacherSubjects ts ON c.class_id = ts.aClass.class_id " +
@@ -28,5 +28,17 @@ public interface TeacherRepo extends JpaRepository<Teacher, Integer > {
     @Query("SELECT subject FROM Subject subject " +
             "JOIN TeacherSubjects ts on subject.subject_id = ts.subject.subject_id " +
             "WHERE ts.aClass.class_id = :classId AND ts.users.email = :email")
-    Subject findTeacherSubject(@Param("classId") int classId , @Param("email") String email);
+    Subject findTeacherSubject(@Param("classId") int classId, @Param("email") String email);
+
+
+    @Query("SELECT u.email , u.name , u.role FROM Users u " +
+            "JOIN Teacher  t on u.id =  t.user.id " +
+             "join TeacherSubjects ts on t.user.email = ts.users.email " +
+            "join Class c on ts.aClass.class_id = c.class_id " +
+            " join ClassSubjects cs on ts.aClass.class_id = cs.aClass.class_id" +
+            " join Subject st on cs.subject.subject_id = st.subject_id " +
+            "WHERE c.semester  = :semester  and c.shift = :shift and st.short_name = :subject_name ")
+    Object findTeacherBySemAndShiftAndSubject(@Param("subject_name") String subject_name ,@Param("semester") int semester , @Param("shift") String shift);
+
+
 }

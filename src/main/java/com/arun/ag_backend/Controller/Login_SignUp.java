d671209a9f83e6bc7f0923130922fd290a84a568
Arun_Bikram_Khatri <khatriarun221@gmail.com>
2023-09-27 22:51:31 +0545

    new cross origin 1

--- src/main/java/com/arun/ag_backend/Controller/Login_SignUp.java
+++ src/main/java/com/arun/ag_backend/Controller/Login_SignUp.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@CrossOrigin(origins = "https://ag.rikeshsilwalkhatri.com.np", allowCredentials = "true")
+@CrossOrigin(origins = "${cross.origin.url}", allowCredentials = "true")
 @RestController
 public class Login_SignUp {
 
